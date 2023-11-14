61fc2efa4a7ea0b56c537c14f02392a2ba067626
Nathan Weston <nweston@fastmail.com>
2023-11-14 11:12:41 -0500

    Add Escape as a shortcut to quit.

--- hdrdiff.py
+++ hdrdiff.py
@@ -297,7 +297,7 @@ if __name__ == "__main__":
             ("View Right Image", lambda: images.select_image(1), [qt.Qt.Key_2]),
             ("View Diff", lambda: images.select_image(2), [qt.Qt.Key_3]),
             ("Normalize/Reset", toggle_normalize, [qt.Qt.Key_N]),
-            ("Quit", window.close, [qt.Qt.Key_Q]),
+            ("Quit", window.close, [qt.Qt.Key_Q, qt.Qt.Key_Escape]),
         ]
     ]
     shortcut_info.setText("Shortcuts:\n" + "\n".join(s.description for s in shortcuts))
